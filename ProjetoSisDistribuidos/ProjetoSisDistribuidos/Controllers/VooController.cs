d8db748d1d5764eeb7734f3369a63ab8e7be7478
RicardoSpernega <ricardo_srd@hotmail.com>
2020-05-01 18:36:59 -0300

    Projeto SistemasDistribuidos

---
+++ ProjetoSisDistribuidos/ProjetoSisDistribuidos/Controllers/VooController.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using ProjetoSisDistribuidos.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoSisDistribuidos.Controllers
+{
+    public class VooController : Controller
+    {
+        private List<Voo> voo = new List<Voo>();
+        // GET: Voo
+        public ActionResult Index()
+        {
+            voo = BuscarJson();
+            ViewBag.ListaCodigo = voo.FirstOrDefault().Codigo;
+            return View("Voos", voo);
+        }
+        [HttpPost]
+        public JsonResult EnviarJson(string codigo)
+        {
+            voo = BuscarJson();
+            Voo aux = voo.Where(x => x.Codigo == codigo).FirstOrDefault();
+            EscreverArquivo(aux);
+            return Json("Gravo arquivo json!", JsonRequestBehavior.AllowGet);
+        }
+        private List<Voo> BuscarJson()
+        {
+            string entrada;
+            using (StreamReader lido = new StreamReader("C:\\Users\\FCAMARA\\Desktop\\voos.json"))
+            {
+                entrada = lido.ReadToEnd().Trim();
+            }
+            return JsonConvert.DeserializeObject<List<Voo>>(entrada);
+        }
+        private void EscreverArquivo(Voo _voo)
+        {
+            string nomeArquivo = @"c:\LogDeVoo\log-" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+            StreamWriter writer = new StreamWriter(nomeArquivo);
+            string output = JsonConvert.SerializeObject(_voo);
+            writer.WriteLine(output);
+            writer.Close();
+        }
+    }
+}
---
+++ ProjetoSisDistribuidos/ProjetoSisDistribuidos/Models/Voo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoSisDistribuidos.Models
+{
+    public class Voo
+    {
+        public string Codigo { get; set; }
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public DateTime Horario { get; set; }
+        public string AuxHorario 
+        {
+            get { return this.Horario.ToString(); }
+            set { this.AuxHorario = AuxHorario;   }         
+        }
+        public string Companhia { get; set; }
+        public string Operando { get; set; }
+    }
+}
